ddfa4169f2d970c5f9d202fd60e2bd8ad3e5e739
rabby4 <mdrabbybd4747@gmail.com>
2023-09-24 20:29:23 +0600

    Installed react, react router, tailwind, daysiUI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <h2 className='text-4xl'>This is text</h2>
+
+    </>
+  )
+}
+
+export default App
